2ac28119beb8df86aa311d0285cff6bb59ef5ed8
unknown <andres17vh@gmail.com>
2023-08-01 11:44:57 -0500

    feat:Values and variables

---
+++ Fundamentals-01/Values-Variables/script.js
@@ -0,0 +1,26 @@
+{/**-------Values and Variables -------*/ }
+
+
+let firstName = 'Pepito';
+let second_name = 'Pinocho';
+let $fistUser = 'Super Man';
+let fruits = ['mango', 'coconut'];
+let firstNumber = 23;
+let secondNumber = 57;
+
+
+{/**-------practice -------*/ }
+
+{/** LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console */}
+
+
+let country = 'colombia';
+let continent = 'America';
+let population = '30000000'
+
+
+let data = `data===> ${country} is on the  ${continent}, it's a number population about ${population} people.`
+console.log(data)
